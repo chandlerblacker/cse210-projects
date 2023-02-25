6fef5ab6763269282c4d47ff6582416a0fd9d8d9
chandlerblacker <109766211+chandlerblacker@users.noreply.github.com>
2023-02-25 08:49:53 -0600

    Add files via upload

---
+++ prove/Develop04/Program.cs
@@ -0,0 +1,47 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool quit = false;
+
+        while(quit == false)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start Breathing Activity");
+            Console.WriteLine("  2. Start Reflecting Activity");
+            Console.WriteLine("  3. Start Listing Activity");
+            Console.WriteLine("  4. Quit");
+            Console.WriteLine("Select a choice from the menu: ");
+            int choice = Convert.ToInt16(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                BreathingActivity b1 = new BreathingActivity();
+                b1.run();
+            }
+
+            if (choice == 2)
+            {
+                ReflectingActivity a1 = new ReflectingActivity();
+                a1.run();
+            }
+
+            if (choice == 3)
+            {
+                BreathingActivity b1 = new BreathingActivity();
+                b1.run();
+            }
+
+            if (choice == 4)
+            {
+                break;
+            }
+        }
+
+
+
+
+    }
+}
