f3c300ca986026c5c0c76bcd34e9e516b28fc252
chandlerblacker <109766211+chandlerblacker@users.noreply.github.com>
2023-03-01 22:21:30 -0600

    Add files via upload

---
+++ prove/Develop05/Eternal.cs
@@ -0,0 +1,23 @@
+public class Eternal : Goal
+{
+    
+    public Eternal(string name, string description, int points) : base (name, description, points)
+    {
+        
+    }
+
+    public override int RecordEvent()
+    {
+        return GetPoints();
+    }
+
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    public override string GetCurrentlyComplete()
+    {
+        return "";
+    }
+}
---
+++ prove/Develop05/Simple.cs
@@ -0,0 +1,25 @@
+public class Simple : Goal
+{
+    bool _complete = false;
+
+
+    public Simple(string name, string description, int points) : base (name, description, points)
+    {
+        
+    }
+
+    public override int RecordEvent()
+    {
+        _complete = true;
+        return GetPoints();
+    }
+
+    public override bool IsComplete()
+    {
+        return _complete;
+    }
+        public override string GetCurrentlyComplete()
+    {
+        return "";
+    }
+}
