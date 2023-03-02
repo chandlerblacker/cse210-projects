840927532ffb703974701c7e9d3877e38d59c3bb
chandlerblacker <109766211+chandlerblacker@users.noreply.github.com>
2023-03-01 18:48:49 -0600

    Add files via upload

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,28 @@
+public abstract class Goal
+{
+    private string _name;
+    private string _description;
+    private int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public string GetPoints()
+    {
+        return _points;
+    }
+}
